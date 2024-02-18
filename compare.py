57afd22cf49c94c8d646ab217ad8c9b476999e6f
romualdas <romualdas.gircys@interstartas.lt>
2024-02-18 23:15:06 +0200

    fix time measure

--- compare.py
+++ compare.py
@@ -50,36 +50,34 @@ def buildGraph(edges):
     return graph 
 
 
-# def measurTime1(type, graph, start, finish):
-#     visited = set()
+def printResults(type, graph, start, finish):
+    visited = set()
 
-#     match type:
-#         case "BFS":
-#             time_taken = timeit.timeit(lambda: hasPathBFS(graph, start, finish, visited))
-#         case "DFS":
-#             time_taken = timeit.timeit(lambda: hasPathDFS(graph, start, finish, visited))
-#         case _:
-#             return 0
-
-#     return time_taken
+    match type:
+        case "DFS":
+            print(f"DFS - has path from '{start}' to '{finish}': {hasPathDFS(graph, start, finish, visited)} | Visited: {len(visited)} | Time: {measureTime("DFS", graph, start, finish)}s")
+        case "BFS":
+            print(f"BFS - has path from '{start}' to '{finish}': {hasPathBFS(graph, start, finish, visited)} | Visited: {len(visited)} | Time: {measureTime("BFS", graph, start, finish)}s")
+        case _:
+            print("ERROR")
 
 
-# def measurTime2(type, graph, start, finish):
-#     visited = set()
+def measureTime(type, graph, start, finish):
+    visited = set()
 
-#     match type:
-#         case "BFS":
-#             start_time = timeit.default_timer()
-#             x = hasPathBFS(graph, start, finish, visited)
-#             end_time = timeit.default_timer()
-#         case "DFS":
-#             start_time = timeit.default_timer()
-#             x = hasPathDFS(graph, start, finish, visited)
-#             end_time = timeit.default_timer()
-#         case _:
-#             return 0
+    match type:
+        case "BFS":
+            start_time = timeit.default_timer()
+            x = hasPathBFS(graph, start, finish, visited)
+            end_time = timeit.default_timer()
+        case "DFS":
+            start_time = timeit.default_timer()
+            x = hasPathDFS(graph, start, finish, visited)
+            end_time = timeit.default_timer()
+        case _:
+            return 0
 
-#     return end_time - start_time
+    return "{:.10f}".format(end_time - start_time)
 
 
 print("\t"+"  x - y")
@@ -98,23 +96,15 @@ edges = [
 ]
 graph = buildGraph(edges)
 
-print("\t\tDFS")
-visited = set()
-print(f"has path from 'x' to 'v': {hasPathDFS(graph, 'x', 'v', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathDFS(graph, 'x', 'v', visited))} s")
-visited = set()
-print(f"has path from 'y' to 'v': {hasPathDFS(graph, 'y', 'v', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathDFS(graph, 'y', 'v', visited))} s")
-visited = set()
-print(f"has path from 'w' to 'x': {hasPathDFS(graph, 'w', 'x', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathDFS(graph, 'w', 'x', visited))} s")
+printResults("DFS", graph, 'x', 'v')
+printResults("BFS", graph, 'x', 'v')
+print("")
+printResults("DFS", graph, 'y', 'v')
+printResults("BFS", graph, 'y', 'v')
+print("")
+printResults("DFS", graph, 'w', 'x')
+printResults("BFS", graph, 'w', 'x')
 
-print("\t\tBFS")
-visited = set()
-print(f"has path from 'x' to 'v': {hasPathBFS(graph, 'x', 'v', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathBFS(graph, 'x', 'v', visited))} s")
-visited = set()
-print(f"has path from 'y' to 'v': {hasPathBFS(graph, 'y', 'v', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathBFS(graph, 'y', 'v', visited))} s")
-visited = set()
-print(f"has path from 'w' to 'x': {hasPathBFS(graph, 'w', 'x', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathBFS(graph, 'w', 'x', visited))} s")
-
-print("________________________________________________\n")
 print("\t"+"i - j")
 print("\t"+"| /")
 print("\t"+"k - l")
@@ -133,23 +123,15 @@ edges = [
 ]
 graph = buildGraph(edges)
 
-print("\t\tDFS")
-visited = set()
-print(f"has path from 'i' to 'm': {hasPathDFS(graph, 'i', 'm', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathDFS(graph, 'i', 'm', visited))} s")
-visited = set()
-print(f"has path from 'o' to 'n': {hasPathDFS(graph, 'o', 'n', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathDFS(graph, 'o', 'n', visited))} s")
-visited = set()
-print(f"has path from 'j' to 'o': {hasPathDFS(graph, 'j', 'o', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathDFS(graph, 'j', 'o', visited))} s")
+printResults("DFS", graph, 'i', 'm')
+printResults("BFS", graph, 'i', 'm')
+print("")
+printResults("DFS", graph, 'o', 'n')
+printResults("BFS", graph, 'o', 'n')
+print("")
+printResults("DFS", graph, 'j', 'o')
+printResults("BFS", graph, 'j', 'o')
 
-print("\t\tBFS")
-visited = set()
-print(f"has path from 'i' to 'm': {hasPathBFS(graph, 'i', 'm', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathBFS(graph, 'i', 'm', visited))} s")
-visited = set()
-print(f"has path from 'o' to 'n': {hasPathBFS(graph, 'o', 'n', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathBFS(graph, 'o', 'n', visited))} s")
-visited = set()
-print(f"has path from 'j' to 'o': {hasPathBFS(graph, 'j', 'o', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathBFS(graph, 'j', 'o', visited))} s")
-
-print("________________________________________________\n")
 print("\t"+"    a")
 print("\t"+"   / \\")
 print("\t"+"  b   d")
@@ -166,23 +148,15 @@ edges = [
 ]
 graph = buildGraph(edges)
 
-print("\t\tDFS")
-visited = set()
-print(f"has path from 'c' to 'e': {hasPathDFS(graph, 'c', 'e', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathDFS(graph, 'c', 'e', visited))} s")
-visited = set()
-print(f"has path from 'e' to 'f': {hasPathDFS(graph, 'e', 'f', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathDFS(graph, 'e', 'f', visited))} s")
-visited = set()
-print(f"has path from 'b' to 'd': {hasPathDFS(graph, 'b', 'd', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathDFS(graph, 'b', 'd', visited))} s")
+printResults("DFS", graph, 'c', 'e')
+printResults("BFS", graph, 'c', 'e')
+print("")
+printResults("DFS", graph, 'e', 'f')
+printResults("BFS", graph, 'e', 'f')
+print("")
+printResults("DFS", graph, 'b', 'd')
+printResults("BFS", graph, 'b', 'd')
 
-print("\t\tBFS")
-visited = set()
-print(f"has path from 'c' to 'e': {hasPathBFS(graph, 'c', 'e', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathBFS(graph, 'c', 'e', visited))} s")
-visited = set()
-print(f"has path from 'e' to 'f': {hasPathBFS(graph, 'e', 'f', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathBFS(graph, 'e', 'f', visited))} s")
-visited = set()
-print(f"has path from 'b' to 'd': {hasPathBFS(graph, 'b', 'd', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathBFS(graph, 'b', 'd', visited))} s")
-
-print("________________________________________________\n")
 print("\t"+"a - b - c - d - e")
 print("\t"+"|   |   |   |   |")
 print("\t"+"f - g - h - i - j")
@@ -238,19 +212,11 @@ edges = [
 ]
 graph = buildGraph(edges)
 
-print("\t\tDFS")
-visited = set()
-print(f"has path from 'a' to 'y': {hasPathDFS(graph, 'a', 'y', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathDFS(graph, 'a', 'y', visited))} s")
-visited = set()
-print(f"has path from 'm' to 'e': {hasPathDFS(graph, 'm', 'e', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathDFS(graph, 'm', 'e', visited))} s")
-visited = set()
-print(f"has path from 'u' to 's': {hasPathDFS(graph, 'u', 's', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathDFS(graph, 'u', 's', visited))} s")
-
-print("\t\tBFS")
-visited = set()
-print(f"has path from 'a' to 'y': {hasPathBFS(graph, 'a', 'y', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathBFS(graph, 'a', 'y', visited))} s")
-visited = set()
-print(f"has path from 'm' to 'e': {hasPathBFS(graph, 'm', 'e', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathBFS(graph, 'm', 'e', visited))} s")
-visited = set()
-print(f"has path from 'u' to 's': {hasPathBFS(graph, 'u', 's', visited)} | Visited: {len(visited)} | Time: {timeit.timeit(lambda: hasPathBFS(graph, 'u', 's', visited))} s")
-
+printResults("DFS", graph, 'a', 'y')
+printResults("BFS", graph, 'a', 'y')
+print("")
+printResults("DFS", graph, 'm', 'e')
+printResults("BFS", graph, 'm', 'e')
+print("")
+printResults("DFS", graph, 'u', 's')
+printResults("BFS", graph, 'u', 's')
